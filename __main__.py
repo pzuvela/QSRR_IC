f3d8f97c957abe7d13d884984dadfaa7c92c88ac
pzuvela <petar@petar-zuvela.com>
2019-09-12 16:15:24 +0800

    Added crude parallelization.

--- __main__.py
+++ __main__.py
@@ -5,6 +5,7 @@ from process import traintest, validate
 from matplotlib import pyplot as plt
 from scipy.stats import norm
 import numpy as np
+from joblib import Parallel, delayed
 
 print('Initiating Input Data and Selected parameters...', end=' ')
 modeldata = pd.read_csv(os.getcwd() + '/data/model_protein.csv')
@@ -20,16 +21,31 @@ max_component = 11
 print('Initiated')
 print('')
 
-models_final_train = list()
+# models_final_train = list()
 models_final_valid = list()
 
-max_iter = 100
+max_iter = 1000
 
+
+# Function to run the training in parallel
+def models_train_parallel_fun(arg_iter):
+    return traintest(modeldata, limxc, limdelc, n_splits, max_component)
+
+
+# Run in parallel
+models_final_train = Parallel(n_jobs=1, verbose=1, backend="threading")(map(delayed(models_train_parallel_fun),
+                                                                        zip(range(max_iter))))
+
+
+
+"""
 for i in range(max_iter):
     # warnings.simplefilter('ignore')
-    print('## Iteration #: ', i)
+    print('## Iteration #: \n', i)
     models_final_train.append(traintest(modeldata, limxc, limdelc, n_splits, max_component))
-    validate(validationdata, models_final_train[i], limxc, limdelc)
+    # validate(validationdata, models_final_train[i], limxc, limdelc)
+
+"""
 
 mre_final = [models_final_train[i][5] for i in range(max_iter)]
 plt.hist(mre_final, bins=20, density=True)
