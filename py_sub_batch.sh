b9c8d87495398c4a3f835d2fa78fb361effe26a6
pzuvela <31347029+pzuvela@users.noreply.github.com>
2019-11-07 11:25:30 +0800

    Create py_sub_batch.sh

---
+++ py_sub_batch.sh
@@ -0,0 +1,44 @@
+## Usage:
+# sh py_sub_batch.sh ncpus counter_init max_iter method max_files
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Usage: sh py_sub_batch.sh ncpus counter_init max_iter method max_files'
+    exit 0
+fi
+
+## Input arguments
+# ncups - number of CPUs
+# counter_init - initial value of the counter for the submission script loop
+# max_iter - maximum interations
+# method - method (gbr, or xgb)
+# max_files - maximum number of jobs to submit at once
+
+# Date & time
+dt=$(date '+%d_%m_%Y_%H_%M_%S')
+
+# Replace the number of CPU cores
+sed -i "16s@.*@ncpus=$1@" py_sub.sh
+
+# Replace the initial counter
+sed -i "19s@.*@p=$2@" py_sub.sh
+
+# Replace lines with submission script filenames
+sed -i "26s@.*@rm -rf \${sub_dir}/\${p}_$4_$3_iters_$dt.sh 2> /dev/null@" py_sub.sh
+sed -i "29s@.*@cat <<EOF >> \${sub_dir}/\${p}_$4_$3_iters_$dt.sh@" py_sub.sh
+
+# Replace the lines with log & error file names
+sed -i "35s@.*@#PBS -e \${err_dir}/\${p}_$4_$3_iters_$dt.err@" py_sub.sh
+sed -i "36s@.*@#PBS -o \${logs_dir}/\${p}_$4_$3_iters_$dt.log@" py_sub.sh
+
+# Replace the line with the python "call"
+sed -i "52s@.*@python3.6 \${file} $3 \${p} \$ncpus $4 no \> \${output}/\${p}_$4_$3_iters_$dt.out@" py_sub.sh
+# sed -i "52s@.*@python3.6 \${file} $3 \${p} \$ncpus $4 no" py_sub.sh
+
+# Replace the line to submit the job sh script
+sed -i "56s@.*@qsub \${sub_dir}/\${p}_$4_$3_iters_$dt.sh@" py_sub.sh
+
+# String with the python filename repeated max_files times
+run_string=$(for (( i=1; i<= $5; i++)); do printf "main.py "; done)
+
+# Run the submission script for max_files times
+sh py_sub.sh $run_string
