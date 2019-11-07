4b5b6277310144e9854a72f9dfbeb508acb6fa0d
pzuvela <31347029+pzuvela@users.noreply.github.com>
2019-11-07 11:50:52 +0800

    Added all the parameter ranges.

--- src/modules/regr.py
+++ src/modules/regr.py
@@ -181,9 +181,12 @@ class RegressionHyperParamOpt:
         # Dictionary of hyper-parameter ranges
         self.params_ranges = {'xgb':  ({'n_est_lb': 10, 'n_est_ub': 500, 'lr_lb': 0.1, 'lr_ub': 0.9,
                                         'max_depth_lb': 1, 'max_depth_ub': 5}),
-                              'gbr':  ({'n_est_lb': 10, 'n_est_ub': 500, 'lr_lb': 0.1, 'lr_ub': 0.9,
-                                        'max_depth_lb': 1, 'max_depth_ub': 5}),
-                              'ada': ({'n_est_lb': 10, 'n_est_ub': 500, 'lr_lb': 0.1, 'lr_ub': 0.9})}
+                              'gbr':  ({'n_est_min': 10, 'n_est_max': 500, 'lr_min': 0.1, 'lr_max': 0.9,
+                                        'max_depth_min': 1, 'max_depth_max': 5}),
+                              'ada': ({'n_est_lb': 300, 'n_est_ub': 1000, 'lr_lb': 0.1, 'lr_ub': 0.9}),
+                              'rfr': ({'n_est_lb': 100, 'n_est_ub': 500, 'max_depth_lb': 8, 'max_depth_ub': 20,
+                                       'min_samples_lb': 1, 'min_samples_ub': 4}),
+                              }
 
     @staticmethod
     def obj_fun(x, opt_list):
