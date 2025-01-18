23215cbf4fb15502f3341ce361b69677301dd4d3
petar <petar@petar-zuvela.com>
2025-01-18 14:29:30 +0800

    -> AD Refactoring continued

--- qsrr_ic/analysis/ad/_ad.py
+++ qsrr_ic/analysis/ad/_ad.py
@@ -1,90 +1,93 @@
+from typing import (
+    Optional,
+    Tuple
+)
+
 from dataclasses import dataclass
 
 import numpy as np
+from numpy import ndarray
+
 import pandas as pd
 
 import matplotlib.pyplot as plt
 
+from qsrr_ic.error_handling import ErrorHandling
+from qsrr_ic.models.qsrr.domain_models import QsrrData
+
 
 @dataclass
-class WilliamsData:
+class ApplicabilityDomainData:
     """
     Data class for encapsulating input data and related parameters for WilliamsModel.
     """
-    x1: np.ndarray  # Training input matrix
-    x2: np.ndarray  # Testing input matrix
-    y1: np.ndarray  # True training output values
-    y1_hat: np.ndarray  # Predicted training output values
-    y2: np.ndarray  # True testing output values
-    y2_hat: np.ndarray  # Predicted testing output values
+
+    data_train: QsrrData  # Training data (descriptors, retention)
+    data_test: Optional[QsrrData]  # Test data (descriptors, retention), optional
+    predictions_train: QsrrData  # Training predictions
+    predictions_train: Optional[QsrrData]  # Test predictions
 
 
-class WilliamsModel:
+class ApplicabilityDomain:
     """
     Model responsible for computing residuals, hat matrices, and critical values with lazy loading.
     """
 
-    def __init__(self, data: WilliamsData):
+    def __init__(self, data: ApplicabilityDomainData):
         """
         Initialize the WilliamsModel with a WilliamsData object.
 
         Args:
-            data (WilliamsData): The input data and parameters for the model.
+            data (ApplicabilityDomainData): The input data and parameters for the model.
         """
-        self._data = data
-        self._hat_matrix = None
-        self._residuals = None
-        self._critical_hat_value = None
+        self.__data: ApplicabilityDomainData = data
+        self.__hat_matrix_train: Optional[ndarray] = None
+        self.__hat_matrix_test: Optional[ndarray] = None
+        self.__residuals_train: Optional[ndarray] = None
+        self.__residuals_test: Optional[ndarray] = None
+        self.__critical_hat = None
 
     @property
-    def hat_matrix(self) -> tuple[np.ndarray, np.ndarray]:
-        """
-        Lazy-loaded property for the hat matrix (leverages).
+    def hat_matrix_train(self) -> ndarray:
+        if self.__hat_matrix_train is None:
+            self.__hat_matrix_train = self._compute_hat_matrix()
+        return self.__hat_matrix_train
 
-        Returns:
-            tuple[np.ndarray, np.ndarray]: Training and testing leverages.
-        """
-        if self._hat_matrix is None:
-            self._hat_matrix = self._compute_hat_matrix()
-        return self._hat_matrix
-
-    @hat_matrix.setter
-    def hat_matrix(self, value):
-        raise AttributeError("The 'hat_matrix' property is read-only and cannot be set.")
+    @hat_matrix_train.setter
+    def hat_matrix_train(self, value):
+        raise ErrorHandling.get_property_value_error("hat_matrix_train")
 
     @property
-    def residuals(self) -> tuple[np.ndarray, np.ndarray]:
-        """
-        Lazy-loaded property for the standardized residuals.
+    def hat_matrix_test(self) -> Optional[ndarray]:
+        if self.__hat_matrix_test is None and self.__data.data_test is not None:
+            self.__hat_matrix_train = self._compute_hat_matrix()
+        return self.__hat_matrix_train
 
-        Returns:
-            tuple[np.ndarray, np.ndarray]: Training and testing residuals.
-        """
-        if self._residuals is None:
-            self._residuals = self._compute_residuals()
-        return self._residuals
+    @hat_matrix_train.setter
+    def hat_matrix_train(self, value):
+        raise ErrorHandling.get_property_value_error("hat_matrix_train")
+
+    @property
+    def residuals(self) -> Tuple[ndarray, Optional[ndarray]]:
+        if self.__residuals is None:
+            self.__residuals = self._compute_residuals()
+        return self.__residuals
 
     @residuals.setter
     def residuals(self, value):
-        raise AttributeError("The 'residuals' property is read-only and cannot be set.")
+        raise ErrorHandling.get_property_value_error("residuals")
 
     @property
-    def critical_hat_value(self) -> float:
-        """
-        Lazy-loaded property for the critical hat value.
+    def critical_hat(self) -> float:
+        if self.__critical_hat is None:
+            self.__critical_hat = self._compute_critical_hat_value()
+        return self.__critical_hat
 
-        Returns:
-            float: Critical leverage value.
-        """
-        if self._critical_hat_value is None:
-            self._critical_hat_value = self._compute_critical_hat_value()
-        return self._critical_hat_value
+    @critical_hat.setter
+    def critical_hat(self, value):
+        raise ErrorHandling.get_property_value_error("critical_hat")
 
-    @critical_hat_value.setter
-    def critical_hat_value(self, value):
-        raise AttributeError("The 'critical_hat_value' property is read-only and cannot be set.")
-
-    def _compute_hat_matrix(self) -> tuple[np.ndarray, np.ndarray]:
+    def _compute_hat_matrix(self) -> Tuple[ndarray, Optional[ndarray]]:
         """
         Compute the training and testing hat matrices.
 
